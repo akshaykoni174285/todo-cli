ee9d30b6a31ede8310d269e5473ab9604731720d
akshaykoni174285 <akshaykoni174285@gmail.com>
2022-12-20 14:31:55 +0530

    env args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+//* [#]  1) how to take command line arguments 
+         // ? 1 so will be using this std::env::args
+// will be having two arguments for a action and another for item    
+// *[]  storing the data in structure based and using hashmaps 
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    // let v1:Vec<String> = env::args().collect();
+    // this will collet the arguments in a vector form 
+    let mut first = env::args().nth(1)
+        .expect("please specify action");
+    let mut second = env::args().nth(2)
+        .expect("please specify item");
+
+    println!("{}", first);
+    println!("{}", second);
+
+
 }
